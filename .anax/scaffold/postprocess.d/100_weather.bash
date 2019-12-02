4d45d7279512efdd2d664421a05ff184788dd53a
Wissam sawah <wesam.sawah@me.com>
2019-12-02 14:54:57 +0100

    Added new version and structured the modules

--- .anax/scaffold/postprocess.d/100_weather.bash
+++ .anax/scaffold/postprocess.d/100_weather.bash
@@ -2,3 +2,17 @@
 #
 # anax/anax-ramverk1-me
 #
+# copy config
+rsync -av vendor/aisa18/weather/config ./
+
+# copy view
+
+rsync -av vendor/aisa18/weather/view ./
+
+# copy src
+
+rsync -av vendor/aisa18/weather/src ./
+
+# copy test
+
+rsync -av vendor/aisa18/weather/test ./
